6ed2f70c7d8b15ed52e3d5cabcac7ae3e868fb9c
DecubaRincon <154654400+DecubaRincon@users.noreply.github.com>
2024-01-15 20:10:28 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -301,7 +301,7 @@ if(localStorage.getItem('visitCount')) {
 
   if (count == 3) {
     // Redirige a la página de registro en la tercera visita
-    window.location.href = './index12.html';
+    window.location.href = '';
   } else if (count == 4) {
     // Activar el otro modal en la cuarta visita
     var otroModal = new bootstrap.Modal(document.getElementById('anuncio-modalimagen'), {});
